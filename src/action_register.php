f8ebf30becbc504815fdbe60b3499fb995450e29
Guilherme Almeida <72418555+theguilhermealmeida@users.noreply.github.com>
2022-05-13 23:37:01 +0100

    started log actions

---
+++ src/action_register.php
@@ -0,0 +1,25 @@
+<?php
+    require_once('database/user.class.php');
+    require_once('database/connection.db.php');
+
+    session_start();
+
+    $db = getDatabaseConnection();
+    $email = $_POST["email"];
+    $fist_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
+    $password = $_POST["password"];
+    $confirm_password = $_POST["confirm_password"];
+
+    
+    if (User::emailInUse($db, $email)) {
+        $referer = '../register.php';
+    }
+    else {
+        if(User::newUser($db, $fist_name, $last_name, $email, $password)) {
+            $referer = '../login.php';} 
+        else {$referer = '../search.php';}
+    }
+
+    header('Location: ' . $referer);
+?>
--- src/templates/account.php
+++ src/templates/account.php
@@ -1,6 +1,14 @@
 <?php
 function output_profile(int $id)
 { ?>
+  <?php
+    require_once('database/connection.db.php');
+    require_once('database/user.class.php');
+
+    $db = getDataBaseConnection();
+    $user = User::getUserById($db, $id);
+  ?>
+
   <section id="profile">
     <img src="https://picsum.photos/200" alt="profile photo">
     <div id="tabs">
@@ -22,12 +30,14 @@ function output_profile(int $id)
         onclick="openProfileTab(event, 'profile-orders')">
         Orders
       </button>
+      <form action="action_logout.php" method="post">
       <button id="logout-button" class="profile-section-button">Logout</button>
+      </form>
     </div>
     <section id="profile-info" class="profile-section">
-      <h3>Name</h3>
-      <p>username</p>
-      <p>Location</p>
+      <h3><?php echo $user->first_name . " " . $user->last_name ?></h3>
+      <p><?php echo $user->email ?></p>
+      <p><?php echo $user->city . ", " . $user->country ?></p>
       <a href="">Change password</a>
     </section>
     <section id="profile-reviews" class="profile-section">
@@ -43,9 +53,9 @@ function output_profile(int $id)
 <?php
 function output_login()
 { ?>
-  <form action="" method="post">
-    <input type="text" placeholder="username">
-    <input type="password" placeholder="password">
+  <form action="action_login.php" method="post">
+    <input type="email" name="email" placeholder="username">
+    <input type="password" name="password" placeholder="password">
     <button class="login">Login</button>
   </form>
   <a href="register.php">Register</a>
@@ -55,10 +65,12 @@ function output_login()
 <?php
 function output_register()
 { ?>
-  <form action="" method="post">
-    <input type="text" placeholder="username">
-    <input type="email" placeholder="email">
-    <input type="password" placeholder="password">
+  <form action="action_register.php" method="post">
+    <input type="email" name="email"  placeholder="email">
+    <input type="text" name="first_name" placeholder="First Name">
+    <input type="text" name="last_name" placeholder="Last Name">
+    <input type="password" name="password" placeholder="password">
+    <input type="password" name="confirm_password" placeholder="confirm password">
     <button class="login">Register</button>
   </form>
   <a href="login.php">Login</a>
