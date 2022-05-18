5cfadcea170363cc341631a41732947e85d0a65d
Guilherme Almeida <72418555+theguilhermealmeida@users.noreply.github.com>
2022-05-18 12:27:21 +0100

    Resolved conflicts

--- src/database/restaurant.class.php
+++ src/database/restaurant.class.php
@@ -3,14 +3,16 @@
 
     class Restaurant{
         public int $id;
+        public int $idUser;
         public string $name;
         public string $opens;
         public string $closes;
         public string $category;
         public string $address;
 
-        public function __construct(int $id, string $name, string $opens, string $closes, string $category,string $address){
+        public function __construct(int $id, int $idUser, string $name, string $opens, string $closes, string $category,string $address){
             $this->id = $id;
+            $this->idUser = $idUser;
             $this->name = $name;
             $this->opens = $opens;
             $this->closes = $closes;
@@ -20,21 +22,28 @@
 
         static function getRestaurants(PDO $db, int $count) : array {
             $stmt = $db->prepare(
-                'SELECT idRestaurant, name, opens, closes, category, address
+                'SELECT idRestaurant, idUser, name, opens, closes, category, address
                  FROM Restaurant
                  LIMIT ?');
             $stmt->execute(array($count));
 
             $restaurants = array();
             while ($restaurant = $stmt->fetch()){
-                $restaurants[] = new Restaurant($restaurant['idRestaurant'],$restaurant['name'],$restaurant['opens'],$restaurant['closes'],$restaurant['category'],$restaurant['address']);
+                $restaurants[] = new Restaurant(
+                    $restaurant['idRestaurant'],
+                    $restaurant['idUser'],
+                    $restaurant['name'],
+                    $restaurant['opens'],
+                    $restaurant['closes'],
+                    $restaurant['category'],
+                    $restaurant['address']);
             }
             return $restaurants;
         }
 
         static function searchRestaurants(PDO $db, string $search, int $count) : array {
             $stmt = $db->prepare(
-                'SELECT idRestaurant, name, opens, closes, category, address
+                'SELECT idRestaurant, idUser, name, opens, closes, category, address
                  FROM Restaurant
                  WHERE name 
                  LIKE ? 
@@ -43,15 +52,23 @@
         
             $restaurants = array();
             while ($restaurant = $stmt->fetch()) {
-              $restaurants[] = new Restaurant($restaurant['idRestaurant'],$restaurant['name'],$restaurant['opens'],$restaurant['closes'],$restaurant['category'],$restaurant['address']);
+              $restaurants[] = new Restaurant(
+                  $restaurant['idRestaurant'],
+                  $restaurant['idUser'],
+                  $restaurant['name'],
+                  $restaurant['opens'],
+                  $restaurant['closes'],
+                  $restaurant['category'],
+                  $restaurant['address']);
             }
         
             return $restaurants;
         }
 
+
         static function getRestaurant(PDO $db, int $id) : Restaurant {
             $stmt = $db->prepare(
-                'SELECT idRestaurant, name, opens, closes, category, address
+                'SELECT idRestaurant, idUser, name, opens, closes, category, address
                  FROM Restaurant
                  WHERE idRestaurant = ?');
             $stmt->execute(array($id));
@@ -60,24 +77,12 @@
         
             return new Restaurant(
                 $restaurant['idRestaurant'],
+                $restaurant['idUser'],
                 $restaurant['name'],
                 $restaurant['opens'],
                 $restaurant['closes'],
                 $restaurant['category'],
                 $restaurant['address']);
         }  
-
-        static function getAverage(PDO $db, int $id) : float {
-            $stmt = $db->prepare(
-                'SELECT avg(rating) as average
-                 FROM Review
-                 WHERE idRestaurant = ?
-                ');
-            $stmt->execute(array($id));
-
-            $rating = $stmt->fetch();
-
-            return $rating['average'];
-        }
 
     }
