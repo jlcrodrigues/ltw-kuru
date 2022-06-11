72eda666941bd9fe632813831e19a6e6f96593ac
José Luís Cunha Rodrigues <joseluiscunharodrigues@gmail.com>
2022-06-11 16:58:31 +0100

    Styled profile info & edit sections

--- src/templates/account.php
+++ src/templates/account.php
@@ -1,10 +1,58 @@
-<?php 
+<?php
 require_once("orders.php");
 require_once("restaurants.php");
 ?>
+
 <?php
-function output_profile_orders(PDO $db, Session $session) { 
-  ?>
+function output_profile_info(User $user)
+{ ?>
+  <h2>Profile</h2>
+  <button onclick="openProfileTab(event, 'profile-edit')">
+    <i class="material-symbols-rounded">edit</i>
+  </button>
+  <hr>
+  <i class="material-symbols-rounded">person</i>
+  <p><?php echo $user->first_name . " " . $user->last_name ?></p>
+  <br>
+  <p>Name</p>
+  <br>
+  <i class="material-symbols-rounded">mail</i>
+  <p><?php echo $user->email ?></p>
+  <br>
+  <p>Email</p>
+  <br>
+  <i class="material-symbols-rounded">call</i>
+  <p><?php echo $user->phone ?></p>
+  <br>
+  <p>Phone Number</p>
+  <br>
+  <i class="material-symbols-rounded">home_pin</i>
+  <p><?php echo $user->address ?></p>
+  <br>
+  <p>Address</p>
+  <?php if ($user->city != null and $user->country != null) { ?>
+    <br>
+    <i class="material-symbols-rounded">map</i>
+    <p><?= $user->city . ", " . $user->country; ?></p>
+    <br>
+    <p>City</p>
+  <?php } ?>
+  <br>
+<?php } ?>
+
+<?php
+function output_profile_reviews()
+{ ?>
+  <h2>Reviews</h2>
+  <hr>
+<?php } ?>
+
+<?php
+function output_profile_orders(PDO $db, Session $session)
+{
+?>
+  <h2>Orders</h2>
+  <hr>
   <h3>Processing</h3>
   <hr>
   <?php
@@ -28,127 +76,146 @@ function output_profile_orders(PDO $db, Session $session) {
 <?php } ?>
 
 <?php
+function output_profile_edit(User $user)
+{ ?>
+  <h2>Profile</h2>
+  <button onclick="openProfileTab(event, 'profile-info')">
+    <i class="material-symbols-rounded">edit</i>
+  </button>
+  <form action="../actions/action_edit_profile.php" method="post" class="profile">
+    <hr>
+    <label for="first_name">
+      <i class="material-symbols-rounded">person</i>
+    </label>
+    <input id="first_name" type="text" name="first_name" value="<?= $user->first_name ?>">
+    <br>
+    <label for="first_name">Name</label>
+    <br>
+
+    <label for="last_name">
+      <i class="material-symbols-rounded">person</i>
+    </label>
+    <input id="last_name" type="text" name="last_name" value="<?= $user->last_name ?>">
+    <br>
+    <label for="last_name">Last name</label>
+    <br>
+
+    <label for="email">
+      <i class="material-symbols-rounded">mail</i>
+    </label>
+    <input id="email" type="email" name="email" value="<?= $user->email ?>">
+    <br>
+    <label for="email">Email</label>
+    <br>
+
+    <label for="phone">
+      <i class="material-symbols-rounded">call</i>
+    </label>
+    <input id="phone" type="text" name="phone" value="<?= $user->phone ?>">
+    <br>
+    <label for="phone">Phone Number</label>
+    <br>
+
+    <label for="address">
+      <i class="material-symbols-rounded">home_pin</i>
+    </label>
+    <input id="address" type="text" name="address" value="<?= $user->address ?>">
+    <br>
+    <label for="address">Address</label>
+    <br>
+
+    <label for="city">
+      <i class="material-symbols-rounded">map</i>
+    </label>
+    <input id="city" type="text" name="city" value="<?= $user->city ?>">
+    <br>
+    <label for="city">City</label>
+    <br>
+
+    <label for="country">
+      <i class="material-symbols-rounded">map</i>
+    </label>
+    <input id="country" type="text" name="country" value="<?= $user->country ?>">
+    <br>
+    <label for="country">Country</label>
+    <br>
+
+    <button type="submit">Save</button>
+  </form>
+
+<?php } ?>
+
+<?php
 function output_profile(Session $session)
 { ?>
   <?php
-    require_once(__DIR__ . '/../database/connection.db.php');
-    require_once(__DIR__ . '/../database/user.class.php');
-    require_once(__DIR__ . '/../database/restaurant.class.php');
-    require_once('restaurants.php');
+  require_once(__DIR__ . '/../database/connection.db.php');
+  require_once(__DIR__ . '/../database/user.class.php');
+  require_once(__DIR__ . '/../database/restaurant.class.php');
+  require_once('restaurants.php');
 
-    $db = getDataBaseConnection();
-    $user = User::getUserById($db, $session->getId());
+  $db = getDataBaseConnection();
+  $user = User::getUserById($db, $session->getId());
   ?>
 
   <section id="profile" class="card">
-    <img src="https://picsum.photos/200" alt="profile photo">
+    <header>
+      <img src="https://picsum.photos/200" alt="profile photo">
+      <h3><?php echo $user->first_name . " " . $user->last_name ?></h3>
+    </header>
     <div id="profile-tabs">
-      <button 
-        id="profile-button" 
-        class="profile-section-button" 
-        onclick="openProfileTab(event, 'profile-info')">
+      <button id="profile-button" class="profile-section-button" onclick="openProfileTab(event, 'profile-info')">
         Profile
       </button>
-      <button 
-        id="reviews-button" 
-        class="profile-section-button" 
-        onclick="openProfileTab(event, 'profile-reviews')">
+      <button id="reviews-button" class="profile-section-button" onclick="openProfileTab(event, 'profile-reviews')">
         Reviews
       </button>
-      <button 
-        id="orders-button" 
-        class="profile-section-button" 
-        onclick="openProfileTab(event, 'profile-orders')">
+      <button id="orders-button" class="profile-section-button" onclick="openProfileTab(event, 'profile-orders')">
         Orders
       </button>
-      <button 
-        id="edit-profile-button" 
-        class="profile-section-button" 
-        onclick="openProfileTab(event, 'profile-edit')">
-        Edit Profile
-      </button>
-      <?php if($session->isOwner($session->getId())) { ?>
-        <button 
-        id="owner-button" 
-        class="profile-section-button" 
-        onclick="openProfileTab(event, 'profile-owner')">
-        My restaurants
-      </button> 
-      <?php } 
-       else { ?>
-        <button 
-        id="not-owner-button" 
-        class="profile-section-button" 
-        onclick="openProfileTab(event, 'profile-not-owner')">
-        Become a Owner
-      </button> 
+      <?php if ($session->isOwner($session->getId())) { ?>
+        <button id="owner-button" class="profile-section-button" onclick="openProfileTab(event, 'profile-owner')">
+          My restaurants
+        </button>
+      <?php } else { ?>
+        <button id="not-owner-button" class="profile-section-button" onclick="openProfileTab(event, 'profile-not-owner')">
+          Become an Owner
+        </button>
       <?php } ?>
-      <button 
-        id="change_password-button" 
-        class="profile-section-button" 
-        onclick="openProfileTab(event, 'profile-change_password')">
+      <button id="change_password-button" class="profile-section-button" onclick="openProfileTab(event, 'profile-change_password')">
         Change Password
       </button>
       <form action="../actions/action_logout.php" method="post">
-      <button id="logout-button" class="profile-section-button">Logout</button>
+        <button id="logout-button" class="profile-section-button">Logout</button>
       </form>
     </div>
     <section id="profile-info" class="profile-section">
-      <h3><?php echo $user->first_name . " " . $user->last_name ?></h3>
-      <p><?php echo $user->email ?></p>
-      <p><?php echo $user->phone ?></p>
-      <p><?php echo $user->address ?></p>
-      <p><?php if ($user->city != null and $user->country != null) {echo $user->city . ", " . $user->country;} 
-               else {echo $user->city . $user->country;}?></p>
+      <?php output_profile_info($user); ?>
     </section>
     <article id="profile-reviews" class="profile-section">
-      <p>Review</p>
+      <?php output_profile_reviews(); ?>
     </article>
     <article id="profile-orders" class="profile-section">
-      <h2>Orders</h2>
-      <?php output_profile_orders($db, $session);?>
+      <?php output_profile_orders($db, $session); ?>
     </article>
     <section id="profile-change_password" class="profile-section">
       <p>Change Password</p>
     </section>
     <section id="profile-edit" class="profile-section">
-      <p>Edit Profile</p>
-    <form action="../actions/action_edit_profile.php" method="post" class="profile">
-      <label for="first_name">First Name:</label>
-      <input id="first_name" type="text" name="first_name" value="<?=$user->first_name?>">
-      
-      <label for="last_name">Last Name:</label>
-      <input id="last_name" type="text" name="last_name" value="<?=$user->last_name?>">  
-
-      <label for="email">Email:</label>
-      <input id="email" type="email" name="email" value="<?=$user->email?>">  
-
-      <label for="address">Address:</label>
-      <input id="address" type="text" name="address" value="<?=$user->address?>">  
-
-      <label for="city">City:</label>
-      <input id="city" type="text" name="city" value="<?=$user->city?>">  
-     
-      <label for="country">Country:</label>
-      <input id="country" type="text" name="country" value="<?=$user->country?>">  
-
-      <label for="phone">Phone:</label>
-      <input id="phone" type="text" name="phone" value="<?=$user->phone?>">  
-      <button type="submit">Save</button>
-    </form>
+      <?php output_profile_edit($user); ?>
     </section>
     <section id="profile-owner" class="profile-section">
-      <h3>Restaurants       <a href="../pages/register_restaurant.php"><button name=add class="add_restaurant"><i class="material-icons">add_circle</i></button></a></h3>
-      <?php 
-        $restaurants = Restaurant::getOwnerRestaurants($db, $session->getId());
-        
-        foreach ($restaurants as $restaurant) {
-          output_restaurant_card_nano($restaurant);
-        }
+      <h3>Restaurants <a href="../pages/register_restaurant.php"><button name=add class="add_restaurant"><i class="material-icons">add_circle</i></button></a></h3>
+      <?php
+      $restaurants = Restaurant::getOwnerRestaurants($db, $session->getId());
+
+      foreach ($restaurants as $restaurant) {
+        output_restaurant_card_nano($restaurant);
+      }
       ?>
     </section>
     <section id="profile-not-owner" class="profile-section">
-      <h3>Become a owner now!</h3> 
+      <h3>Become a owner now!</h3>
       <?php output_register_restaurant_form($db, $session) ?>
     </section>
   </section>
@@ -176,14 +243,13 @@ function output_register()
     <form id="register" action="../actions/action_register.php" method="post">
       <input type="text" name="first_name" placeholder="First Name">
       <input type="text" name="last_name" placeholder="Last Name">
-      <input type="email" name="email"  placeholder="Email">
+      <input type="email" name="email" placeholder="Email">
       <input type="password" name="password" placeholder="Password">
       <input type="password" name="confirm_password" placeholder="Confirm Password">
       <button type="submit" name="submit" class="login">Register</button>
     </form>
-      <hr>
-      <a href="login.php">I have an account</a>
+    <hr>
+    <a href="login.php">I have an account</a>
   </div>
-  <?php
-  }
-
+<?php
+}
